35cdbf4320304774abc39f2a5ee7fd96119d0e94
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-05 15:52:19 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup,find_package
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='ytdlpy',
     version='0.0.1',
