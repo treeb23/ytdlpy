3916485522fcafb08acdb1aa56e7782c37ec9ec2
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-05 16:45:10 +0900

    Update ytdlpy.py

--- ytdlpy/ytdlpy.py
+++ ytdlpy/ytdlpy.py
@@ -1,3 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import wave
+import sys
+import IPython.display
+from IPython.display import display
+import librosa.display
+import ffmpeg
+from pydub import AudioSegment
+import simpleaudio
+from youtube_transcript_api import YouTubeTranscriptApi
+from yt_dlp import YoutubeDL
+import librosa
+import nltk
+
 def fromDLtoCSV(URL): # 字幕ダウンロードから音声分割まで一括実行（JSON書き込み含む）
     print("[fromDLtoCSVを実行]")
     text,text_jp,start,duration,_=yt_totext(URL)
