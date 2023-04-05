ce874e6a1cd5a70a42daca36820fab57c2f781c0
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-05 16:07:02 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup,find_packages
-install_requires = [
-    ffmpeg
-    pydub
-    simpleaudio
-    youtube-transcript-api
-    yt-dlp
-    librosa
-    nltk
-]
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
@@ -18,5 +9,4 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=install_requires
 )
