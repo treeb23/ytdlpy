6d2528130a1d5215d6d43897dfa6d97dad6e7d23
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-04-22 16:21:56 +0900

    Update ytdlpy.py

--- ytdlpy/ytdlpy.py
+++ ytdlpy/ytdlpy.py
@@ -539,7 +539,7 @@ def set_trialLSTM(f_path, df, wordindex, ep=50, embdim=100, trainrate=0.7, model
                 a += 1
     print("predict(train) : ", a / traindata_num)
     
-    torch.save(model, f'{f_path}/{modelpath}.pth')
+    torch.save(model.state_dict(), f'{f_path}/{modelpath}.pth')
     
     return losses
 
@@ -554,7 +554,38 @@ def testmodel(f_path,df,wordindex,modelpath,text):
     def sentence2index(sentence):
         sentence=sentence.split(' ')
         return torch.tensor([word2index[w] for w in sentence], dtype=torch.long)
-    model = torch.load(f'{f_path}/{modelpath}.pth')
+    
+    class LSTMClassifier(nn.Module):
+        # モデルで使う各ネットワークをコンストラクタで定義
+        def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
+            # 親クラスのコンストラクタ。決まり文句
+            super(LSTMClassifier, self).__init__()
+            # 隠れ層の次元数。これは好きな値に設定しても行列計算の過程で出力には出てこないので。
+            self.hidden_dim = hidden_dim
+            # インプットの単語をベクトル化するために使う
+            self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
+            # LSTMの隠れ層。これ１つでOK。超便利。
+            self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+            # LSTMの出力を受け取って全結合してsoftmaxに食わせるための１層のネットワーク
+            self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+            # softmaxのLog版。dim=0で列、dim=1で行方向を確率変換。
+            self.softmax = nn.LogSoftmax(dim=1)
+
+        # 順伝播処理はforward関数に記載
+        def forward(self, sentence):
+            # 文章内の各単語をベクトル化して出力。2次元のテンソル
+            embeds = self.word_embeddings(sentence)
+            # 2次元テンソルをLSTMに食わせられる様にviewで３次元テンソルにした上でLSTMへ流す。
+            # 上記で説明した様にmany to oneのタスクを解きたいので、第二戻り値だけ使う。
+            _, lstm_out = self.lstm(embeds.view(len(sentence), 1, -1))
+            # lstm_out[0]は３次元テンソルになってしまっているので2次元に調整して全結合。
+            tag_space = self.hidden2tag(lstm_out[0].view(-1, self.hidden_dim))
+            # softmaxに食わせて、確率として表現
+            tag_scores = self.softmax(tag_space)
+            return tag_scores
+    
+    model = LSTMClassifier()
+    model.load_state_dict(torch.load(f'{f_path}/{modelpath}.pth'))
     with torch.no_grad():
         # テストデータの予測
         inputs = sentence2index(text)
