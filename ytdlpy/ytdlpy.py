21300aadd5bfdfabfe594370ecb098eb1c2619e2
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-06-24 03:01:42 +0900

    Update ytdlpy.py

--- ytdlpy/ytdlpy.py
+++ ytdlpy/ytdlpy.py
@@ -1004,9 +1004,11 @@ def plotwav(video_num:int,sentence_num:int,word_num:int,f_path,df_csv):
     end=int(float(m["word_endtime"][w])*16000)
     print(f"{v_id}/{v_id}_{x}.wav, {start/16000}~{end/16000}[s]")
     p=pd.DataFrame(wav)[0][start:end]
+    word=m["word"][w]
+    maxvol=m["maxvol"][w]
     plt.plot(p)
     plt.show()
-    return m,p,v_id # m:指定した文のdf, p:単語の音量配列
+    return m,p,v_id,word,maxvol # m:指定した文のdf, p:単語の音量配列
 
 # 動画の全単語の音量の確率分布を調べる
 def viewdist(f_path,df_csv,i):
