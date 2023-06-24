a387afee029163d57ebe23c787ede62625603779
treeb23 <105105490+treeb23@users.noreply.github.com>
2023-06-24 14:21:06 +0900

    Update ytdlpy.py

--- ytdlpy/ytdlpy.py
+++ ytdlpy/ytdlpy.py
@@ -1002,10 +1002,11 @@ def plotwav(video_num:int,sentence_num:int,word_num:int,f_path,df_csv,wavplot=Tr
     m=txt[txt["sentence_num"]==x]
     start=int(float(m["word_starttime"][w])*16000)
     end=int(float(m["word_endtime"][w])*16000)
-    print(f"{v_id}/{v_id}_{x}.wav, {start/16000}~{end/16000}[s]")
+    print(f"{v_id}/{v_id}_{x}.wav, {start/16000}~{end/16000}[s], {start}~{end}")
     p=pd.DataFrame(wav)[0][start:end]
     word=m["word"][w]
     maxvol=m["maxvol"][w]
+    print(f"{m["sentence"][w]}, {word}, max: {maxvol}")
     if wavplot==True:
         plt.plot(p)
         plt.show()
